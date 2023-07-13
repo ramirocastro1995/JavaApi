bc1302e3c97288fcacf1719395d2baa43a42e0e9
ramirocastro1995 <ramirosalvadorcastro95@hotmail.com>
2023-07-13 19:50:31 -0300

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(new Student(
+				1l,
+				"Ramiro",
+				"ramiro@gmail.com",
+				LocalDate.of(1995, Month.MAY,9),
+				21
+		));
+	}
+}
