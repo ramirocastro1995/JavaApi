9b79f8658b2af568d58a68bb48f4c1a02214b38e
ramirocastro1995 <ramirosalvadorcastro95@hotmail.com>
2023-07-16 13:12:53 -0300

    Repository and service

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +9,20 @@ import java.time.Month;
 import java.util.List;
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-    return List.of(new Student(
-                1l,
-                        "Ramiro",
-                        "ramiro@gmail.com",
-                LocalDate.of(1995, Month.MAY,9),
-                21
-                        ));
+        return studentRepository.findAll();
+//    return List.of(new Student(
+//                1l,
+//                        "Ramiro",
+//                        "ramiro@gmail.com",
+//                LocalDate.of(1995, Month.MAY,9),
+//                21
+//                        ));
     }
 }
